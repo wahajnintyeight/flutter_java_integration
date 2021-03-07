29ae482150ab566cfa38609920ec9a1f476ebd98
Muhammad Wahaj <wahaj.dkz5598@ucp.edu.pk>
2021-03-07 19:47:07 +0500

    Interface

---
+++ Flutter_Java/counter_flutter/lib/Screens/Home/constant/data_json.dart
@@ -0,0 +1,119 @@
+List menuItems = [
+  "Bag",
+  "Shoes",
+  "Shirt",
+  "Watch",
+  "Dress",
+  "Skirt",
+  "Pant",
+  "Wallet",
+  "Backpack",
+  "Jean"
+];
+
+List dataItems = [
+  {
+    "id": 1,
+    "code": "CS667880",
+    "name":
+        "667880 Sexy tube top dress summer new fashion bag hip big code nightclub womens club party princess",
+    "img": "images/earrings.jpg",
+    "price": 15.5,
+    "promotionPrice": 11.50,
+    "color": [
+      {"id": 1, "value": "images/earrings.jpg"},
+      {"id": 2, "value": "images/earrings.jpg"}
+    ],
+    "size": [
+      {"id": 1, "value": "S"},
+      {"id": 2, "value": "M"},
+      {"id": 3, "value": "L"}
+    ]
+  },
+  {
+    "id": 2,
+    "code": "CS883398",
+    "name":
+        "883398 Hong Kong style retro fashion temperament summer new casual wild sexy waist was thin",
+    "img": "images/earrings.jpg",
+    "price": 18.00,
+    "promotionPrice": 15.00,
+    "color": [
+      {"id": 1, "value": "images/earrings.jpg"},
+      {"id": 2, "value": "images/earrings.jpg"}
+    ],
+    "size": [
+      {"id": 1, "value": "FREE-SIZE"}
+    ]
+  },
+  {
+    "id": 3,
+    "code": "CS757458",
+    "name":
+        "757458 Temperament dress summer sexy nightclub dress v-neck slim slimming irregular hips sleeveless",
+    "img": "images/earrings.jpg",
+    "price": 16.5,
+    "promotionPrice": 12.5,
+    "color": [
+      {"id": 1, "value": "images/earrings.jpg"},
+      {"id": 2, "value": "images/earrings.jpg"}
+    ],
+    "size": [
+      {"id": 1, "value": "S"},
+      {"id": 2, "value": "M"},
+      {"id": 3, "value": "L"}
+    ]
+  },
+  {
+    "id": 4,
+    "code": "CS605084",
+    "name":
+        "605084 Super fairy fairy skirt net red retro thin waist tie bow two wear sexy strap cake dress",
+    "img": "images/earrings.jpg",
+    "price": 12.6,
+    "promotionPrice": 11.00,
+    "color": [
+      {"id": 1, "value": "images/earrings.jpg"},
+      {"id": 2, "value": "images/earrings.jpg"}
+    ],
+    "size": [
+      {"id": 1, "value": "FREE-SIZE"}
+    ]
+  },
+  {
+    "id": 5,
+    "code": "CP555545",
+    "name": "555545 Autumn and winter high waist jeans",
+    "img": "images/earrings.jpg",
+    "price": 17.00,
+    "promotionPrice": 13.00,
+    "color": [
+      {"id": 1, "value": "images/earrings.jpg"},
+      {"id": 2, "value": "images/earrings.jpg"}
+    ],
+    "size": [
+      {"id": 1, "value": "25"},
+      {"id": 2, "value": "26"},
+      {"id": 3, "value": "27"},
+      {"id": 4, "value": "28"},
+    ]
+  },
+  {
+    "id": 6,
+    "code": "CP598703",
+    "name": "598703 Waist large size loose casual Slim thin feet pants",
+    "img": "images/earrings.jpg",
+    "price": 14.5,
+    "promotionPrice": 17.00,
+    "color": [
+      {"id": 1, "value": "images/earrings.jpg"},
+      {"id": 2, "value": "images/earrings.jpg"}
+    ],
+    "size": [
+      {"id": 1, "value": "25"},
+      {"id": 2, "value": "26"},
+      {"id": 3, "value": "27"},
+      {"id": 4, "value": "28"},
+    ]
+  }
+];
--- Flutter_Java/counter_flutter/lib/Screens/Home/mainpage.dart
+++ Flutter_Java/counter_flutter/lib/Screens/Home/mainpage.dart
@@ -9,9 +9,12 @@ import 'package:counter_flutter/Screens/Welcome/Retailer/Welcome/retailwelcome.d
 import 'package:counter_flutter/Screens/Home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:counter_flutter/Screens/Welcome/Customer/Welcome/welcome.dart';
-
+import 'package:line_icons/line_icons.dart';
+import 'package:counter_flutter/Screens/Home/constant/data_json.dart';
 import 'home.dart';
+import 'package:counter_flutter/Screens/Home/theme/colors.dart';
 import 'home.dart';
+import 'package:counter_flutter/Screens/Home/productdetail.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -22,9 +25,6 @@ final int jID = 0;
 
 class MainPageCus extends StatefulWidget {
   final JewelryType jT;
-
-// final int jID;
-//   MainPageCus(this.jT);
 
   const MainPageCus({Key key, this.jT}) : super(key: key);
 
@@ -48,27 +48,161 @@ class _MainPageCusState extends State<MainPageCus> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    int activeMenu = 0;
     return Scaffold(
-        body: Padding(
-            padding: EdgeInsets.all(10),
-            child: ListView(
+        // body: Padding(
+        //     padding: EdgeInsets.all(10),
+        //     child: ListView(
+        //       children: <Widget>[
+        //         SizedBox(height: size.height * 0.05),
+        //         Container(
+        //             alignment: Alignment.center,
+        //             padding: EdgeInsets.all(10),
+        //             child: Text(
+        //               "Shop 'N' Preview",
+        //               style: TextStyle(
+        //                   color: Colors.blue,
+        //                   fontWeight: FontWeight.w500,
+        //                   fontSize: 30),
+        //             )),
+        //         SizedBox(height: size.height * 0.05),
+        //         Text(widget.jT.jewelryType),
+        //         SizedBox(height: size.height * 0.05),
+        //         Text(widget.jT.jID.toString())
+        //       ],
+        //     )));
+        body: SafeArea(
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                SizedBox(height: size.height * 0.05),
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      "Shop 'N' Preview",
-                      style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30),
-                    )),
-                SizedBox(height: size.height * 0.05),
-                Text(widget.jT.jewelryType),
-                SizedBox(height: size.height * 0.05),
-                Text(widget.jT.jID.toString())
+                Text(
+                  "Online Shopping",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(LineIcons.search),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(LineIcons.shoppingCart)
+                  ],
+                )
               ],
-            )));
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20, left: 20),
+              child: Row(
+                  children: List.generate(menuItems.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        activeMenu = index;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: activeMenu == index
+                                      ? primary
+                                      : Colors.transparent,
+                                  width: 2))),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          menuItems[index],
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              })),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Wrap(
+            children: List.generate(dataItems.length, (index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => ProductDetailPage(
+                                id: dataItems[index]['id'].toString(),
+                                name: dataItems[index]['name'],
+                                code: dataItems[index]['code'],
+                                img: dataItems[index]['img'],
+                                price: dataItems[index]['price'].toString(),
+                                promotionPrice: dataItems[index]
+                                        ['promotionPrice']
+                                    .toString(),
+                                size: dataItems[index]['size'],
+                                color: dataItems[index]['color'],
+                              )));
+                },
+                child: Card(
+                    elevation: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Hero(
+                          tag: dataItems[index]['id'].toString(),
+                          child: Container(
+                            width: (size.width - 16) / 2,
+                            height: (size.width - 16) / 2,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage(dataItems[index]['img']),
+                                    fit: BoxFit.cover)),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text(
+                            dataItems[index]['code'],
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text(
+                            dataItems[index]['price'].toString() + " USD",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    )),
+              );
+            }),
+          )
+        ],
+      ),
+    ));
   }
 }
